3c9fdcad8c2818016105e7eb04da4e0fd5039848
savan2073 <savan2073@gmail.com>
2023-01-28 17:50:03 +0100

    rece mnie bola

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Container } from "@mui/material"
 import './App.css';
-//import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-//import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar';
+import Top100 from './Pages/Top100/Top100';
+import Movies from './Pages/Movies/Movies';
+import Series from './Pages/Series/Series';
+import Search from './Pages/Search/Search';
 
 function App() {
   return (
-    <>
-      
+    <BrowserRouter>
+    <Navbar/>
       <div className="App">
-        <Home/>
+        <Container>
+            <Routes>
+              <Route exact path="/" element={<Home/>}/>
+              <Route path="/series" element={<Series/>}/>
+              <Route path="/movies" element={<Movies/>}/>
+              <Route path="/top_100" element={<Top100/>}/>
+              <Route path="/search" element={<Search/>}/>
+            </Routes>
+        </Container>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
