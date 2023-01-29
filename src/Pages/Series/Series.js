670e003125cfdcb868eac93af2bfaa5357005c15
savan2073 <savan2073@gmail.com>
2023-01-29 16:49:52 +0100

    series view

--- src/Pages/Series/Series.js
+++ src/Pages/Series/Series.js
@@ -1,10 +1,62 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Genres from "../../components/Genres/Genres";
+import CustomPage from "../../components/Page/CustomPage";
+import SingleContent from "../../components/SingleContent/SingleContent"
+import useGenre from "../../hooks/useGenre";
 
 
 const Series = () =>{
+
+    const [page, setPage] = useState(1);
+    const [content, setContent] = useState([]);
+    const [numOfPages, setNumOfPages] = useState();
+    const [selectedGenres, setSelectedGenres] = useState([]);
+    const [genres, setGenres] = useState([]);
+    const genreforURL = useGenre(selectedGenres)
+
+    const fetchMovies = async() =>{
+        const { data } = await axios.get(
+            `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_watch_monetization_types=flatrate&with_genres=${genreforURL}`
+        );
+        setContent(data.results);
+        console.log(data)
+        setNumOfPages(data.total_pages)
+    };
+
+    useEffect(() => {
+        window.scroll(0, 0);
+        fetchMovies()
+    }, [page, genreforURL]);
+
+
     return(
         <div>
-            <span className="pageTitle">Series</span>
-            <img width="100%" src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=2000"></img>
+            <div className="trending">
+                <span className="pageTitle">Series</span>
+            <Genres
+            type = "tv"
+            selectedGenres = {selectedGenres}
+            setSelectedGenres ={setSelectedGenres}
+            genres = {genres}
+            setGenres ={setGenres}
+            setPage = {setPage}
+            />
+                {content && content.map((c) => (
+                    <SingleContent
+                    key={c.id}
+                    id={c.id}
+                    poster={c.poster_path}
+                    title={c.title || c.name}
+                    date={c.first_air_date || c.release_date}
+                    media_type="tv"
+                    vote_average={c.vote_average}
+                  />
+                ))}
+            </div>
+            {numOfPages > 1 && (
+        <CustomPage setPage={setPage} numOfPages={50} />
+      )}
         </div>
     )
 }
